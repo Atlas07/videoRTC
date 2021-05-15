1d5d199f600f68e52ee8ea708b706a432d3bf5d7
Vitalii Shapoval <vitalii_shapoval@ukr.net>
2021-05-15 17:15:59 +0300

    feat: add video RTC stream

---
+++ public/script.js
@@ -0,0 +1,22 @@
+let myVideoStream;
+const videoGrid = document.getElementById("video-grid");
+const myVideo = document.createElement("video");
+
+const addVideoStream = (video, stream) => {
+  video.srcObject = stream;
+  video.addEventListener("loadedmetadata", () => {
+    video.play();
+    videoGrid.append(video);
+  });
+};
+
+myVideo.muted = true;
+navigator.mediaDevices
+  .getUserMedia({
+    audio: true,
+    video: true,
+  })
+  .then((stream) => {
+    myVideoStream = stream;
+    addVideoStream(myVideo, stream);
+  });
